4a1fcd11dd6f3b252fd0aca923d97ed87e4494ee
Ray Reyes <rayreyes5@gmail.com>
2019-09-04 02:48:40 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,13 +1,13 @@
 def new_hash
-  # return an empty hash
+(new_hash).to eq({0})
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+ (my_hash).to eq({1})
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+ {:name => "Grace Hopper")
 end
 
 def id_generator
